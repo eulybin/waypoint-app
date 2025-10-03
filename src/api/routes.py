b112ec648d8225621b414925e6bb1c691923b760
Victor Moreno Cabello <victormorenocabello@gmail.com>
2025-10-03 21:27:07 +0200

    empezando los endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,12 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import bcrypt 
+import requests
+import os
+from datetime import datetime
+import json
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +18,22 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Autentificacion
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/register', methods=['POST'])
+def register_user():
+    try: 
+        data = request.get_json()
+        
+        # VALIDACION BASICA
+        
+        if not data or not data.get('name') or not data.get('email') or not data.get('password'):
+            return jsonify({"message": "Faltan datos: name, email, password"}), 400
+        
+        email = data.get('email')
+        password = data.get('password')
+        name = data.get('name')
+        
+        #Validacion si ya existe
+        
+        
