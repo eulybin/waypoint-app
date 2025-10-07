7525d32baae5819b9656bcddc989f24c0204cd85
Egor Ulybin <egorulybin@gmail.com>
2025-10-07 11:10:20 +0200

    dark mode theme applied

--- src/front/hooks/useGlobalReducer.jsx
+++ src/front/hooks/useGlobalReducer.jsx
@@ -1,10 +1,17 @@
-import { useContext, useReducer, createContext } from "react";
+import { useEffect, useContext, useReducer, createContext } from "react";
 import storeReducer, { initialStore } from "../store"
 
 const StoreContext = createContext()
 
 export function StoreProvider({ children }) {
     const [store, dispatch] = useReducer(storeReducer, initialStore())
+
+    useEffect(() => {
+        const theme = store.isDarkMode ? "dark" : "light"
+        document.documentElement.setAttribute("data-bs-theme", theme)
+        localStorage.setItem("isDarkMode", JSON.stringify(store.isDarkMode))
+    }, [store.isDarkMode])
+
     return <StoreContext.Provider value={{ store, dispatch }}>
         {children}
     </StoreContext.Provider>
