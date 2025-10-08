1a472fd82de5153655460f1519dbc89c1989a985
Egor Ulybin <egorulybin@gmail.com>
2025-10-08 19:45:19 +0200

    refactored report and thank you modals to separate componenets

--- src/front/components/Modals/ReportModal.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom"
-
-const ReportProblemModal = ({ children }) => {
-    const portalRoot = document.getElementById("report-portal-root")
-    return ReactDOM.createPortal(children, portalRoot)
-}
-
-export default ReportProblemModal
-
