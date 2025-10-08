512e01f98746d3176f7330f6f4ee0e3345823cfc
Egor Ulybin <egorulybin@gmail.com>
2025-10-08 23:04:44 +0200

    polished report problem service

--- src/front/services/reportProblemService.js
+++ src/front/services/reportProblemService.js
@@ -1,19 +1,27 @@
 // (POST) Send Report a Problem Form Data to Backend
-export const reportProblem = async (formDataObj) => {
-  // using FormData object does not require headers prop
+export const reportProblem = async (formDataObj, signal) => {
   const requestOptions = {
     method: 'POST',
     body: formDataObj,
+    signal,
   };
   try {
     const response = await fetch('url', requestOptions);
-    if (!response.ok) {
-      throw new Error('Could not send the form data from Report a Problem modal.');
+    let data;
+    try {
+      data = await response.json();
+    } catch {
+      data = { message: 'Server returned invalid JSON.' };
     }
-    //need to make sure that the backend returns json, otherwise we get an ERROR here...
-    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || 'Could not send the form data from Report a Problem modal.');
+    }
     return data;
   } catch (error) {
-    console.error(error);
+    if (error.name === 'AbortError') {
+      console.warn('The request was aborted by the user or it timed out.');
+    }
+    console.error('Error in reportProblem:', error);
+    throw error;
   }
 };
