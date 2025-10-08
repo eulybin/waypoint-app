147cdfe217d9dd0fbb4c410f115dcc3d0ed3532d
Egor Ulybin <egorulybin@gmail.com>
2025-10-09 00:30:22 +0200

    comment out horizontal navbar in layout component

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,12 +1,12 @@
-import Navbar from "../components/Navbar"
 import HorizontalNavbar from "../components/HorizontalNavbar"
+import Navbar from "../components/Navbar"
 import { Outlet } from "react-router-dom/dist"
 
 export const Layout = () => {
     return (
         <>
-            {/* <Navbar /> */}
-            <HorizontalNavbar />
+            {/* <HorizontalNavbar /> */}
+            <Navbar />
             <Outlet />
         </>
 
