98ba18aadf73bad58556406904958d08c8e955f7
Egor Ulybin <egorulybin@gmail.com>
2025-10-09 20:10:06 +0200

    created dummy global state to quickly switch between views

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,12 +1,15 @@
 import HorizontalNavbar from "../components/HorizontalNavbar"
 import Navbar from "../components/Navbar"
 import { Outlet } from "react-router-dom/dist"
+import useGlobalReducer from "../hooks/useGlobalReducer"
 
 export const Layout = () => {
+
+    const { store } = useGlobalReducer()
+
     return (
         <>
-            {/* <HorizontalNavbar /> */}
-            <Navbar />
+            {store.userIsLoggedIn ? <Navbar /> : <HorizontalNavbar />}
             <Outlet />
         </>
 
