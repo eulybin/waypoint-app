65532fcc5c739a9817e016c60dcd5519e6d11dcd
Egor Ulybin <egorulybin@gmail.com>
2025-10-09 20:10:06 +0200

    created dummy global state to quickly switch between views

--- src/front/store.js
+++ src/front/store.js
@@ -7,6 +7,8 @@ export const initialStore = () => {
 
   return {
     isDarkMode: localStorageDarkMode ? JSON.parse(localStorageDarkMode) : false,
+    // TEMPORARY SOLUTION to component render based on whether a user has logged in or not
+    userIsLoggedIn: true,
   };
 };
 
