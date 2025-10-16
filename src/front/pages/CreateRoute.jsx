ef0d91b7fe1ba015e485153a38564f5f14600eb3
Egor Ulybin <egorulybin@gmail.com>
2025-10-16 13:56:03 +0200

    reorganized project structure

--- src/front/pages/CreateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CreateRoute = () => {
-    return (
-        <div className='text-center'>Create Route</div>
-    );
-};
-
-export default CreateRoute;
--- src/front/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Home = () => {
-
-	return (
-		<div className="text-center pt-5 display-1">Home Component</div>
-	);
-}; 
